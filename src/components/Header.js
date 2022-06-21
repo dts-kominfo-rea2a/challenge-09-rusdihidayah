6aabf05932d472c33cc0f2cc76ad5e3d9afe9fd3
Rusdi Hidayah <rusdi.hidayah11@gmail.com>
2022-06-21 15:29:34 +0700

    Intro React

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import "../components/header.css";
 
 const Header = () => {
     return (
-        <></>
+        <>
+            <div className='header'>
+                <h1 className='title'>Call a Friend</h1>
+                <h2 className='subtitle'>Your Contact App</h2>
+            </div>
+        </>
     )
 }
 
