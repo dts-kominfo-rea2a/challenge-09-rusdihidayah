45323d445d3eabd4b6718a2f43aaca165b4471e1
Rusdi Hidayah <rusdi.hidayah11@gmail.com>
2022-06-21 15:56:16 +0700

    Intro React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import './App.css';
-import Header from './components/Header';
-import Contact from './components/Contact';
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      <Header>
+      <Header />
       {contacts.map((d, i) => (
-          <Contact key={i} data={d} />
-        ))}
-      </Header>
-      
+        <Contact key={i} data={d} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
-import "../components/header.css";
+import React from "react";
+import "../components/Header.css";
 
 const Header = () => {
-    return (
-        <>
-            <div className='header'>
-                <h1 className='title'>Call a Friend</h1>
-                <p className='subtitle'>Your Contact App</p>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Call a Friend</h1>
+        <p className="subtitle">Your Friendly Contact App</p>
+      </div>
+    </>
+  );
+};
 
 export default Header;
